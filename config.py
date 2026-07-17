dd15d5bc0c06664d04c3edb0ae5d7780879e798d
sebastiancarrascogz <sebx23@gmail.com>
2026-07-17 14:51:46 -0400

    ✨ (config.py, config.yaml, ingest_bronze.py): introduce YAML configuration for pipeline dates to enhance flexibility and maintainability ♻️ (ingest_bronze.py): refactor date range loading to use configuration values instead of hardcoded dates

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
 import os
+import yaml
 
 load_dotenv()
 
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 BIGQUERY_PROJECT_ID = os.getenv("BIGQUERY_PROJECT_ID")
+PIPELINE_START_DATE = config["pipeline"]["start_date"]
+PIPELINE_END_DATE = config["pipeline"]["end_date"]
--- dlt/ingest_bronze.py
+++ dlt/ingest_bronze.py
@@ -4,8 +4,9 @@ import dlt
 from dlt.sources.helpers import requests
 import pandas as pd
 from typing import Generator
+from config import PIPELINE_START_DATE, PIPELINE_END_DATE
 
-@dlt.resource
+@dlt.resource(write_disposition="append")
 def load_parquet_date_range(start_date: datetime.datetime, end_date: datetime.datetime) -> Generator[pd.DataFrame, None, None]:
     for date in pd.date_range(start=start_date, end=end_date, freq='MS'):
         url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{date.strftime('%Y-%m')}.parquet"
@@ -22,8 +23,8 @@ pipeline = dlt.pipeline(
 if __name__ == "__main__":
     load_info = pipeline.run(
         load_parquet_date_range(
-            start_date=datetime.datetime(2024, 1, 1),
-            end_date=datetime.datetime(2024, 2, 1)
+            start_date=datetime.datetime.strptime(PIPELINE_START_DATE, "%Y-%m"),
+            end_date=datetime.datetime.strptime(PIPELINE_END_DATE, "%Y-%m")
         )
     )
     print(load_info)
