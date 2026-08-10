24f80df69594e1e98280fd60d0f61d59cf62320e
sebastiancarrascogz <sebx23@gmail.com>
2026-08-10 02:23:35 -0400

    ✨ (dbt_project.yml, agg_taxi_trips_daily.sql, fct_taxi_trips.sql, yellow_trips.sql): add trip_duration_max variable and calculate trip duration in minutes to enhance data analysis capabilities ⬆️ (pyproject.toml): bump version to 0.0.11 to reflect recent changes and improvements

--- dbt/nyc_yellow_taxi/models/silver/yellow_trips.sql
+++ dbt/nyc_yellow_taxi/models/silver/yellow_trips.sql
@@ -6,10 +6,11 @@ TO_HEX(MD5(CONCAT(CAST(yt.vendor_id AS STRING),
     CAST(yt.tpep_dropoff_datetime AS STRING),
     CAST(yt.pu_location_id AS STRING),
     CAST(yt.do_location_id AS STRING)
-))) as trip_id, -- 1. Crear PK
+))) as trip_id, 
 vi.name as vendor_name,
 yt.tpep_pickup_datetime,
 yt.tpep_dropoff_datetime,
+timestamp_diff(yt.tpep_dropoff_datetime, yt.tpep_pickup_datetime, second)/60.0 as trip_duration_minutes,
 cast(yt.passenger_count as int64) as passenger_count,         
 yt.trip_distance,           
 ri.name as ratecode_name,              
@@ -22,42 +23,43 @@ pu_zones.borough as pu_borough_name,
 case when do_zones.borough = "N/A" then "Outside of NYC" else do_zones.borough end as do_borough_name,
 pt.name as payment_type_name,       
 yt.fare_amount,     
-case when yt.fare_amount < 0 then True else False end as is_fare_negative, -- .5             
+case when yt.fare_amount < 0 then True else False end as is_fare_negative,          
 yt.extra,                   
-case when yt.extra < 0 then True else False end as is_extra_negative, -- .6             
+case when yt.extra < 0 then True else False end as is_extra_negative,            
 yt.mta_tax,                 
-case when yt.mta_tax < 0 then True else False end as is_mta_tax_negative, -- .7
+case when yt.mta_tax < 0 then True else False end as is_mta_tax_negative, 
 yt.tip_amount,
-case when yt.tip_amount < 0 then True else False end as is_tip_negative, -- .8              
+case when yt.tip_amount < 0 then True else False end as is_tip_negative,              
 yt.tolls_amount,            
-case when yt.tolls_amount < 0 then True else False end as is_tolls_negative, -- .9
+case when yt.tolls_amount < 0 then True else False end as is_tolls_negative, 
 yt.improvement_surcharge,   
-case when yt.improvement_surcharge < 0 then True else False end as is_improvement_surcharge_negative, -- .10
+case when yt.improvement_surcharge < 0 then True else False end as is_improvement_surcharge_negative,
 yt.total_amount,            
-case when yt.total_amount < 0 then True else False end as is_total_amount_negative, -- .11
+case when yt.total_amount < 0 then True else False end as is_total_amount_negative, 
 yt.congestion_surcharge,    
-case when yt.congestion_surcharge < 0 then True else False end as is_congestion_surcharge_negative, -- .12
+case when yt.congestion_surcharge < 0 then True else False end as is_congestion_surcharge_negative, 
 yt.airport_fee,             
-case when yt.airport_fee < 0 then True else False end as is_airport_fee_negative -- .13
+case when yt.airport_fee < 0 then True else False end as is_airport_fee_negative 
 from {{source('bronze', 'yellow_trips')}} yt
 left join {{ref('vendor_id')}} vi on yt.vendor_id = vi.id
 left join {{ref('payment_type')}} pt on yt.payment_type = pt.id
-left join {{ref('ratecode_id')}} ri on coalesce(yt.ratecode_id, 99) = ri.id -- 4. Imputar nulos a 99
+left join {{ref('ratecode_id')}} ri on coalesce(yt.ratecode_id, 99) = ri.id 
 left join {{ref('taxi_zone_lookup')}} pu_zones on yt.pu_location_id = pu_zones.location_id
 left join {{ref('taxi_zone_lookup')}} do_zones on yt.do_location_id = do_zones.location_id
-where yt.pu_location_id not in (264,265) and yt.do_location_id <> 264 -- 0. Filtro de locationIDs
-and yt.tpep_pickup_datetime < yt.tpep_dropoff_datetime -- 1. Inconsistencia de fechas
+where yt.pu_location_id not in (264,265) and yt.do_location_id <> 264 
 and yt.tpep_pickup_datetime >= '2009-01-01' and yt.tpep_dropoff_datetime >= '2009-01-01'
 and  yt.tpep_pickup_datetime <= current_timestamp() and yt.tpep_dropoff_datetime <= current_timestamp()
-and yt.passenger_count >= 1 and yt.passenger_count < 6 -- 2. Pasajeros
-and yt.trip_distance > 0 and yt.trip_distance < 30 -- 3. Filtro de distancia 
-and yt.fare_amount >= -140 and yt.fare_amount <= 140 and yt.fare_amount <> 0 -- .5
-and yt.extra >= -15 and yt.extra <= 15 -- .6
-and yt.mta_tax >= -{{var('silver')['mta_tax_max']}} and yt.mta_tax <= {{var('silver')['mta_tax_max']}} -- .7 (Parametrizado)
-and yt.tip_amount >= -100 and yt.tip_amount <= 100 -- .8
-and yt.tolls_amount >= -35 and yt.tolls_amount <= 35 -- .9
-and yt.improvement_surcharge >= -{{var('silver')['improvement_surcharge_max']}} and yt.improvement_surcharge <= {{var('silver')['improvement_surcharge_max']}} -- .10
-and yt.total_amount >= -200 and yt.total_amount <= 200 and yt.total_amount <> 0 -- .11
-and coalesce(yt.congestion_surcharge, 0) >= -{{var('silver')['congestion_surcharge_max']}} and coalesce(yt.congestion_surcharge, 0) <= {{var('silver')['congestion_surcharge_max']}} -- .12
-and coalesce(yt.airport_fee, 0) >= -{{var('silver')['airport_fee_max']}} and coalesce(yt.airport_fee, 0) <= {{var('silver')['airport_fee_max']}} -- .13
+and timestamp_diff(yt.tpep_dropoff_datetime, yt.tpep_pickup_datetime, second) > 0 -- Filtra DO < PU 
+and timestamp_diff(yt.tpep_dropoff_datetime,yt.tpep_pickup_datetime, second) <= {{ var('silver')['trip_duration_max'] }} * 60
+and yt.passenger_count >= 1 and yt.passenger_count < 6 
+and yt.trip_distance > 0 and yt.trip_distance < 30 
+and yt.fare_amount >= -140 and yt.fare_amount <= 140 and yt.fare_amount <> 0 
+and yt.extra >= -15 and yt.extra <= 15 
+and yt.mta_tax >= -{{var('silver')['mta_tax_max']}} and yt.mta_tax <= {{var('silver')['mta_tax_max']}} 
+and yt.tip_amount >= -100 and yt.tip_amount <= 100 
+and yt.tolls_amount >= -35 and yt.tolls_amount <= 35 
+and yt.improvement_surcharge >= -{{var('silver')['improvement_surcharge_max']}} and yt.improvement_surcharge <= {{var('silver')['improvement_surcharge_max']}} 
+and yt.total_amount >= -200 and yt.total_amount <= 200 and yt.total_amount <> 0 
+and coalesce(yt.congestion_surcharge, 0) >= -{{var('silver')['congestion_surcharge_max']}} and coalesce(yt.congestion_surcharge, 0) <= {{var('silver')['congestion_surcharge_max']}} 
+and coalesce(yt.airport_fee, 0) >= -{{var('silver')['airport_fee_max']}} and coalesce(yt.airport_fee, 0) <= {{var('silver')['airport_fee_max']}} 
 QUALIFY ROW_NUMBER() OVER (PARTITION BY trip_id ORDER BY tpep_pickup_datetime) = 1 -- unicidad 
