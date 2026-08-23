320c84e3b35f887388ff7b7434265228527ea77b
sebastiancarrascogz <sebx23@gmail.com>
2026-08-23 13:49:00 -0400

    ✨ (agg_pickup_zone_daily.sql, agg_taxi_trips_daily.sql, agg_taxi_trips_hourly.sql): change materialization strategy to incremental for better performance and efficiency 💡 (agg_pickup_zone_daily.sql, agg_taxi_trips_daily.sql, agg_taxi_trips_hourly.sql): add comments to indicate dependencies on yellow_trips model for clarity ✅ (agg_pickup_zone_daily.sql, agg_taxi_trips_daily.sql, agg_taxi_trips_hourly.sql): implement logic to handle incremental updates based on affected dates to optimize data processing

--- dbt/nyc_yellow_taxi/models/gold/agg_pickup_zone_daily.sql
+++ dbt/nyc_yellow_taxi/models/gold/agg_pickup_zone_daily.sql
@@ -1,27 +1,56 @@
-{{config(materialized='table')}}
+-- depends_on: {{ ref('yellow_trips') }}
 
-with pickup_zone_daily as (
-    select 
-pu_location_id, -- Campo relevante para el mapa
-count(*) as trip_count, -- Campo relevante para el mapa
-date(tpep_pickup_datetime) as pickup_date,
+{{ config(
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
+    partition_by={
+        "field": "pickup_date",
+        "data_type": "date",
+        "granularity": "day"
+    }
+) }}
+
+with
+
+{% if is_incremental() %}
+
+affected_dates as (
+select distinct date(tpep_pickup_datetime) as pickup_date
+from {{ ref('yellow_trips') }}
+where source_file_month = date('{{ var("batch_month") }}')
+),
+
+{% endif %}
+
+pickup_zone_daily as (
+select
+pu_location_id,
+count(*) as trip_count,
+pickup_date,
 pu_borough_name,
 do_borough_name,
 trip_distance_categ,
 payment_type_name,
 passenger_count as trip_passenger_count
-from {{ref('fct_taxi_trips')}} 
-group by pickup_date, 
+from {{ ref('fct_taxi_trips') }}
+
+{% if is_incremental() %}
+where pickup_date in (select pickup_date from affected_dates)
+{% endif %}
+
+group by
+pickup_date,
 pu_borough_name,
-pu_location_id, 
+pu_location_id,
 do_borough_name,
 trip_distance_categ,
 payment_type_name,
 trip_passenger_count
 )
-select 
+
+select
 p.*,
 d.zone_name as pu_zone_name,
 d.geometry
-from pickup_zone_daily p 
-left join {{ref('dim_taxi_zones')}} d on p.pu_location_id = d.location_id
+from pickup_zone_daily p
+left join {{ ref('dim_taxi_zones') }} d on p.pu_location_id = d.location_id
--- dbt/nyc_yellow_taxi/models/gold/agg_taxi_trips_hourly.sql
+++ dbt/nyc_yellow_taxi/models/gold/agg_taxi_trips_hourly.sql
@@ -1,15 +1,38 @@
-{{config(materialized='table')}}
+-- depends_on: {{ ref('yellow_trips') }}
+{{ config(
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
+    partition_by={
+        "field": "pickup_date",
+        "data_type": "date",
+        "granularity": "day"
+    }
+) }}
+with 
+{% if is_incremental() %}
 
+affected_dates as (
+    select distinct
+        date(tpep_pickup_datetime) as pickup_date
+    from {{ ref('yellow_trips') }}
+    where source_file_month = date('{{ var("batch_month") }}')
+),
+
+{% endif %}
+agg_hourly as (
 select 
 extract(hour from tpep_pickup_datetime) as pickup_hour,
 count(*) as trip_count,
-date(tpep_pickup_datetime) as pickup_date,
+pickup_date,
 pu_borough_name,
 do_borough_name,
 trip_distance_categ,
 payment_type_name,
 passenger_count as trip_passenger_count
 from {{ref('fct_taxi_trips')}}
+{% if is_incremental() %}
+where pickup_date in (select pickup_date from affected_dates)
+{% endif %}
 group by pickup_hour, 
 pickup_date, 
 pu_borough_name, 
@@ -17,3 +40,6 @@ do_borough_name,
 trip_distance_categ,
 payment_type_name,
 trip_passenger_count
+)
+select *
+from agg_hourly
