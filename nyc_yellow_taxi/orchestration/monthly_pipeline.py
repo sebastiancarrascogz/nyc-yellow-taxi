573048966a654afa13770e90c5ca49d6615962d8
sebastiancarrascogz <sebx23@gmail.com>
2026-08-30 00:01:35 -0400

    ✨ (monthly_pipeline.py): add function to calculate target batch month for ingestion ♻️ (monthly_pipeline.py): refactor monthly_taxi_pipeline to use calculated target month if not provided 📝 (monthly_pipeline.py): update main execution block to deploy the pipeline with Prefect

--- nyc_yellow_taxi/orchestration/monthly_pipeline.py
+++ nyc_yellow_taxi/orchestration/monthly_pipeline.py
@@ -1,13 +1,21 @@
 import datetime
 import subprocess
 from pathlib import Path
-from prefect import flow, task
-from nyc_yellow_taxi.ingestion.load_yellow_trips import ingest_yellow_trips
 
+from dateutil.relativedelta import relativedelta
+from prefect import flow, task
+
+from nyc_yellow_taxi.ingestion.load_yellow_trips import ingest_yellow_trips
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DBT_PROJECT_DIR = PROJECT_ROOT / "dbt" / "nyc_yellow_taxi"
 
+def get_target_batch_month() -> str:
+    today = datetime.date.today()
+    current_month = today.replace(day=1)
+    target_month = current_month - relativedelta(months=2)
+
+    return target_month.isoformat()
 
 # 1. Ingesta
 @task(retries=2, retry_delay_seconds=60)
@@ -38,11 +46,18 @@ def run_dbt_tests() -> None:
     )
 
 @flow
-def monthly_taxi_pipeline(batch_month: str) -> None:
-    ingest_month(batch_month) # 1. Ingesta (Bronze)
-    run_dbt_transformations(batch_month) # 2. Transformaciones dbt (Silver, Gold)
-    run_dbt_tests() # 3. Tests de dbt 
+def monthly_taxi_pipeline(batch_month: str | None = None) -> None:
+    batch_month = batch_month or get_target_batch_month()
+    ingest_month(batch_month)
+    run_dbt_transformations(batch_month)
+    run_dbt_tests()
 
 
 if __name__ == "__main__":
-    monthly_taxi_pipeline("2024-06-01")
+    monthly_taxi_pipeline.from_source(
+        source=str(Path(__file__).resolve().parents[2]),
+        entrypoint="nyc_yellow_taxi/orchestration/monthly_pipeline.py:monthly_taxi_pipeline",
+    ).deploy( # pyright: ignore[reportAttributeAccessIssue]
+        name="monthly-taxi-local",
+        work_pool_name="local-process-pool",
+    )
