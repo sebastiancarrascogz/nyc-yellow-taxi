247b703925b0dc0fc2896c70e339b7ecb60ad0af
sebastiancarrascogz <sebx23@gmail.com>
2026-09-08 22:15:53 -0300

    ✨ (dbt_project.yml, cleanup_retention.sql, agg_pickup_zone_daily.sql, vw_pickup_zone_daily_map.sql): introduce new retention variables for detail and aggregate months to enhance data management ♻️ (cleanup_retention.sql): refactor retention logic to utilize new variables for improved clarity and maintainability 📝 (pyproject.toml): update version to 0.0.24 to reflect recent changes and improvements

--- dbt/nyc_yellow_taxi/models/gold/agg_pickup_zone_daily.sql
+++ dbt/nyc_yellow_taxi/models/gold/agg_pickup_zone_daily.sql
@@ -15,42 +15,50 @@ with
 {% if is_incremental() %}
 
 affected_dates as (
-select distinct date(tpep_pickup_datetime) as pickup_date
-from {{ ref('yellow_trips') }}
-where source_file_month = date('{{ var("batch_month") }}')
+
+    select distinct
+        date(tpep_pickup_datetime) as pickup_date
+
+    from {{ ref('yellow_trips') }}
+
+    where source_file_month = date('{{ var("batch_month") }}')
+
 ),
 
 {% endif %}
 
 pickup_zone_daily as (
-select
-pu_location_id,
-count(*) as trip_count,
-pickup_date,
-pu_borough_name,
-do_borough_name,
-trip_distance_categ,
-payment_type_name,
-passenger_count as trip_passenger_count
-from {{ ref('fct_taxi_trips') }}
 
-{% if is_incremental() %}
-where pickup_date in (select pickup_date from affected_dates)
-{% endif %}
+    select
+        pu_location_id,
+        count(*) as trip_count,
+        pickup_date,
+        pu_borough_name,
+        do_borough_name,
+        trip_distance_categ,
+        payment_type_name,
+        passenger_count as trip_passenger_count
 
-group by
-pickup_date,
-pu_borough_name,
-pu_location_id,
-do_borough_name,
-trip_distance_categ,
-payment_type_name,
-trip_passenger_count
+    from {{ ref('fct_taxi_trips') }}
+
+    {% if is_incremental() %}
+
+    where pickup_date in (
+        select pickup_date
+        from affected_dates
+    )
+
+    {% endif %}
+
+    group by
+        pickup_date,
+        pu_borough_name,
+        pu_location_id,
+        do_borough_name,
+        trip_distance_categ,
+        payment_type_name,
+        trip_passenger_count
 )
 
-select
-p.*,
-d.zone_name as pu_zone_name,
-d.geometry
-from pickup_zone_daily p
-left join {{ ref('dim_taxi_zones') }} d on p.pu_location_id = d.location_id
+select *
+from pickup_zone_daily
